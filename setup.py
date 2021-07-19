cb1caa78f16a3bbea0067f4fffe91298e9d165ee
Juan Farias <juanfarias580@gmail.com>
2021-07-19 14:40:30 -0300

    add url in file setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="juanfarias580@gmail.com",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/JuanFariasGit/utils_formatter_br_package.git",
     packages=find_packages(),
     python_requires='>=3.8',
 )
